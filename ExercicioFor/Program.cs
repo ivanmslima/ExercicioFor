3a542f2f765f889a854ed3018803145e456adaf7
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-28 21:04:06 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for(int i=numero; i>0; i=i -numero / 5)
+            {
+                Console.Write(" " +i);
+            }
+
+
+        }
+    }
+}
